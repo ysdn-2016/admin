0530f23e2c742fa4d0ac578c6262c3f403a006c7
Ross Zurowski <ross@rosszurowski.com>
2016-01-11 09:54:54 -0500

    Fix login with return key

--- lib/views/login.vue
+++ lib/views/login.vue
@@ -3,7 +3,7 @@
 		<p class="login-form-error callout error" v-if="error.message">{{ error.message }}</p>
 		<input type="email" class="login-form-email" placeholder="Email" v-model="email" />
 		<input type="password" class="login-form-password" placeholder="Password" v-model="password" />
-		<a class="button primary login-button" type="submit" @click.prevent="login">Log In</a>
+		<button class="button primary login-button" type="submit" @click.prevent="login">Log In</button>
 		<p class="login-form-help">Forgot your password? Ping <a href="https://www.facebook.com/messages/100007107395716">Ross</a> or <a href="https://www.facebook.com/messages/791492565">Taylor</a> and we'll help you out.</p>
 	</form>
 </template>
