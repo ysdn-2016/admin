466977b701dc2c42e9d717cc18d54fea35bddf05
Ross Zurowski <ross@rosszurowski.com>
2016-02-27 15:51:04 -0500

    Add side notes

--- lib/components/editor.vue
+++ lib/components/editor.vue
@@ -1,6 +1,6 @@
 <template>
-	<div class="editor">
-		<header class="editor-action-bar">
+	<div class="editor" :class="{ 'editor--no-controls': !controls, 'editor--character-limit': limit, 'editor--character-limit-error': limitError }">
+		<header class="editor-action-bar" v-if="controls">
 			<div class="editor-actions">
 				<span @click.prevent="enableEditor" class="editor-action editor-markdown-action" :class="{ 'active': !previewing }">Write</span>
 				<span @click.prevent="enablePreview" class="editor-action editor-preview-action" :class="{ 'active': previewing }">Preview</span>
@@ -11,7 +11,10 @@
 			 :rows="size"
 			 v-el:editor
 			 v-model="content"
-			 v-show="!previewing">{{ contents }}</textarea>
+			 v-show="!previewing"
+			 @keyup="limitLength"
+			 @blur="limitLength">{{ contents }}</textarea>
+		<div class="editor-limit" v-if="limit" v-show="!previewing"><span class="editor-limit-number" title="Remaining characters">{{ remainingCharacterCount }}</span></div>
 		<div class="editor-preview" v-el:preview v-if="previewing" v-html="renderedContent | markdown | default 'Nothing to preview'"></div>
 	</div>
 </template>
@@ -22,6 +25,7 @@ import autosize from 'autosize'
 import fitvids from 'fitvids'
 
 import linkAssets from '../filters/assets'
+import markdownCharacterCount from '../helpers/markdown-character-count'
 
 export default {
 
@@ -40,15 +44,23 @@ export default {
 			type: String,
 			default: ''
 		},
+		controls: {
+			type: Boolean,
+			default: true
+		},
 		size: {
 			type: Number,
 			default: 8
+		},
+		limit: {
+			type: Number
 		}
 	},
 
 	data () {
 		return {
-			previewing: false
+			previewing: false,
+			limitError: false
 		}
 	},
 
@@ -57,9 +69,13 @@ export default {
 	},
 
 	computed: {
-		renderedContent: function () {
+		renderedContent () {
 			if (!this.assets.length) return this.content
 			return linkAssets(this.content, this.assets)
+		},
+		remainingCharacterCount () {
+			if (!this.limit) return
+			return this.limit - markdownCharacterCount(this.content)
 		}
 	},
 
@@ -81,6 +97,12 @@ export default {
 		},
 		enablePreview () {
 			this.previewing = true
+		},
+		limitLength (e) {
+			this.limitError = false
+			if (!this.limit) return
+			if (markdownCharacterCount(this.content) <= this.limit) return
+			this.limitError = true
 		}
 	}
 
--- lib/views/profile.vue
+++ lib/views/profile.vue
@@ -20,11 +20,21 @@
 				<p>This email will be used on the site to let people contact you. It is also the email you log in with.<br/>Changing this will change your login information.</p>
 			</div>
 		</div>
-		<div class="form-field profile-biography-field">
-			<label class="form-field-label" for="biography">About</label>
-			<editor :content.sync="biography" placeholder="Write a little bit about yourself"></editor>
+		<div class="form-field profile-email-field">
+			<label class="form-field-label" for="intro">Brief Introduction</label>
+			<editor :content.sync="introduction" :limit="maxIntroLength" :size="2" placeholder="Who are you? Share one or two lines about yourself."></editor>
+			<div class="field-help-text">
+				<p>This text will be shown on <a href="http://{{ domain }}/{{ name | slug }}" target="_blank">your profile</a> and alongside your picture on any projects.</p>
+			</div>
 		</div>
-		<div class="profile-social-links">
+		<div class="form-field profile-biography-field">
+			<label class="form-field-label" for="biography">Biography</label>
+			<editor :content.sync="biography" :limit="maxBioLength" :size="6" placeholder="What is your story? Share your journey as a designer, what you’re interested in, or what you hope to do after you graduate."></editor>
+			<div class="field-help-text">
+				<p>This text will only be shown on <a href="http://{{ domain }}/{{ name | slug }}" target="_blank">your profile</a>.</p>
+			</div>
+		</div>
+		<div class="form-field profile-social-links">
 			<!-- Uploaded CV -->
 			<label class="form-field-label">Social Links</label>
 			<div class="form-field-row">
@@ -57,6 +67,28 @@
 				<p>Provide a full link (http://yoursite.com) to your website, and your username for the other networks.<br/>Are we missing an important network? <a href="https://www.facebook.com/messages/100007107395716">Let us know</a>.</p>
 			</div>
 		</div>
+		<div class="form-field profile-side-notes">
+			<label class="form-field-label">Side Notes</label>
+			<div class="field-help-text profile-side-notes-help-text">
+				<p>Side notes are extra pieces of information on <a href="http://{{ domain }}/{{ name | slug }}" target="_blank">your profile</a> that you'd like to share with site visitors.<br>Pick 2&ndash;4 categories to be shown on your profile.</p>
+			</div>
+			<div class="profile-side-notes-options">
+				<div class="profile-side-notes-option" v-for="note in sideNotes">
+					<div class="profile-side-notes-option-header">
+						<div class="profile-side-notes-option-checkbox"
+							:class="{ 'profile-side-notes-option-checkbox--enabled': note.enabled }"
+							@click.prevent="toggleNote(note)"></div>
+						<div class="profile-side-notes-option-description">
+							<div class="profile-side-notes-option-label" @click.prevent="toggleNote(note)">{{ note.label }}</div>
+							<div class="profile-side-notes-option-help">{{ findSideNoteConfig(note).help }}</div>
+						</div>
+					</div>
+					<div class="profile-side-notes-option-editor" v-if="note.enabled">
+						<editor :content.sync="note.contents" :size="2" :limit="maxSideNoteLength" placeholder="Write&hellip;" :controls="false"></editor>
+					</div>
+				</div>
+			</div>
+		</div>
 		<footer class="project-form-footer">
 			<div class="project-form-footer-context">
 				<a v-link="{ name: 'home' }" class="button">Cancel</a>
@@ -75,17 +107,34 @@ import extend from 'extend'
 
 import api from '../api'
 import auth from '../auth'
+import config from '../config'
 import router from '../router'
+
+import markdownCharacterCount from '../helpers/markdown-character-count'
 
 import Editor from '../components/editor.vue'
 
 const isEmail = str => /\S+@\S+/i.test(str)
 const isURL = str => new RegExp('(http|ftp|https)://', 'i').test(str)
 const hasLength = str => !!str.trim().length
+const clone = arr => arr.slice()
 
 const error = err => {
 	alert('An error occured. Open the console (⌘+opt+J), take a screen shot, and send it to Ross. (Sorry for the inconvenience)')
 	console.error(err)
+}
+
+const defaultSideNotes = () => {
+	const allSideNotes = clone(config.content.sideNotes)
+	const defaults = allSideNotes.map(note => {
+		return {
+			id: note.id,
+			label: note.label,
+			contents: '',
+			enabled: false
+		}
+	})
+	return defaults
 }
 
 export default {
@@ -102,7 +151,9 @@ export default {
 			name: user.name,
 			email: user.email,
 			url: user.url,
+			introduction: user.introduction || '',
 			biography: user.biography || '',
+			sideNotes: user.sideNotes || defaultSideNotes(),
 			url: user.url || '',
 			twitter: user.twitter || '',
 			instagram: user.instagram || '',
@@ -116,15 +167,39 @@ export default {
 		valid () {
 			if (!hasLength(this.email)) return false
 			if (!isEmail(this.email)) return false
+
+			// Social Links
 			if (hasLength(this.url) && !isURL(this.url)) return false
-			// Fail if social is a URL, we just want the username
 			if (hasLength(this.twitter) && isURL(this.twitter)) return false
 			if (hasLength(this.instagram) && isURL(this.instagram)) return false
 			if (hasLength(this.linkedin) && isURL(this.linkedin)) return false
 			if (hasLength(this.dribbble) && isURL(this.dribbble)) return false
 			if (hasLength(this.github) && isURL(this.github)) return false
+
+			// Side Notes
+			const enabledSideNotesCount = this.sideNotes.filter(n => n.enabled).length
+			if (enabledSideNotesCount > config.api.maxSideNoteCount) return false
+			const hasBlankButEnabledSideNote = this.sideNotes.some(n => n.enabled && !hasLength(n.contents))
+			const hasSideNoteLongerThanCharacterCount = this.sideNotes.some(n => n.enabled && markdownCharacterCount(n.contents) > config.api.maxProfileSideNoteCharacterCount)
+			if (hasBlankButEnabledSideNote) return false
+			if (hasSideNoteLongerThanCharacterCount) return false
+
 			return true
 		},
+
+		maxIntroLength () {
+			return config.api.maxProfileIntroductionCharacterCount
+		},
+		maxBioLength () {
+			return config.api.maxProfileBiographyCharacterCount
+		},
+		maxSideNoteLength () {
+			return config.api.maxProfileSideNoteCharacterCount
+		},
+
+		domain () {
+			return config.api.domain
+		}
 	},
 
 	methods: {
@@ -134,7 +209,9 @@ export default {
 				id: auth.user.id,
 				name: this.name,
 				email: this.email,
+				introduction: this.introduction,
 				biography: this.biography,
+				sideNotes: this.sideNotes,
 				url: this.url,
 				twitter: this.twitter,
 				instagram: this.instagram,
@@ -146,6 +223,17 @@ export default {
 				auth.user.authenticated = true
 				router.go({ name: 'home' })
 			}).catch(error)
+		},
+
+		findSideNoteConfig (note) {
+			return config.content.sideNotes.find(n => note.id === n.id)
+		},
+
+		toggleNote (note) {
+			const enabledCount = this.sideNotes.filter(n => n.enabled).length
+			const futureCount = enabledCount + (note.enabled ? -1 : 1)
+			if (futureCount > config.api.maxSideNoteCount) return
+			note.enabled = !note.enabled
 		}
 	}
 
